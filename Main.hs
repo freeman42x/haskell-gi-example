4493d9be808256b716bf308b0c62f1e0d733c8b7
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-05-10 05:59:54 +0100

    Added haskell-gi Main.hs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,26 @@
-module Main where
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+{- cabal:
+build-depends: base, haskell-gi-base, gi-gtk
+-}
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [ #title := "Hi there" ]
+
+  on win #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [ #label := "Click me" ]
+
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+
+  #add win button
+
+  #showAll win
+
+  Gtk.main
